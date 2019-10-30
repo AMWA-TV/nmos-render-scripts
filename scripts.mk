f5078394c95304947e9eab6cf07f4a3331507dad
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-10-30 19:52:31 +0000

    Added scripts.mk

---
+++ scripts.mk
@@ -0,0 +1,27 @@
+.PHONY: build source-repo docs indexes fix-links layouts server push clean
+
+build: source-repo docs indexes fix-links layouts
+
+source-repo:
+	.scripts/get-source-repo.sh
+
+docs:
+	.scripts/extract-docs.sh
+
+indexes:
+	.scripts/make-indexes.sh
+
+fix-links:
+	.scripts/fix-links.sh
+
+layouts:
+	.scripts/make-layouts.sh
+
+server:
+	.scripts/run-server.sh
+
+push:
+	.scripts/push-to-github.sh
+
+clean:
+	.scripts/make-clean.sh
