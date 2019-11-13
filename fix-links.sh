cfb4f08c2a4afd82cf12663424cf96eb6953e408
Peter Brightwell <peter.brightwell@bbc.co.uk>
2019-11-13 11:53:30 +0000

    .json links in docs convert to .html

--- fix-links.sh
+++ fix-links.sh
@@ -25,6 +25,9 @@ for file in {branches,tags}/*/docs/*.md; do
     # Change .raml links to .html and rename APIs folder
     perl -pi -e 's:\.raml\):.html\):g; s:/APIs:/html-APIs:g;' "$file"
 
+    # Change .json links to .html and use with-refs for schemas
+    perl -pi -e 's:\.json\):.html\):g; s:/html-APIs/schemas:/html-APIs/schemas/with-refs:g;' "$file"
+
     # Change %20 escaped spaces in links to understores
     perl -ni -e '@parts = split /(\(.*?\.md\))/ ; for ($n = 1; $n < @parts; $n += 2) { $parts[$n] =~ s/%20/_/g; }; print @parts' "$file"
 
