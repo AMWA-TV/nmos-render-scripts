a07b6d2697a9a604622bb816e7f41eaebd38b499
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-15 19:57:49 +0000

    Use SPEC_SERVER from config

--- upload-site.sh
+++ upload-site.sh
@@ -5,8 +5,7 @@
 
 # The $SSH_ env variables are provided by GitHub secret
 
-# This assumes that the spec is served from a URL that matches the SSH host
-server_dir="/var/www/$SSH_HOST/$SITE_NAME"
+dest="/var/www/$SPEC_SERVER/$SITE_NAME"
 
 if [[ -e .ssh ]]; then
   echo Temp .ssh already exists: exiting
@@ -18,12 +17,13 @@ chmod 700 .ssh
 echo "$SSH_PRIVATE_KEY" > .ssh/id_rsa && chmod 600 .ssh/id_rsa
 echo "$SSH_KNOWN_HOSTS" > .ssh/known_hosts && chmod 600 .ssh/known_hosts
 
+ssh_opts="-i .ssh/id_rsa -o UserKnownHostsFile=.ssh/known_hosts"
+
 echo Uploading
-scp -r -i .ssh/id_rsa -o UserKnownHostsFile=.ssh/known_hosts _site "$SSH_USER@$SSH_HOST:$server_dir.new"
+scp "$ssh_opts" -r _site "$SSH_USER@$SSH_HOST:$dest.new"
 
 echo Replacing old site
-ssh -i .ssh/id_rsa -o UserKnownHostsFile=.ssh/known_hosts "$SSH_USER@$SSH_HOST" \
-    "mv $server_dir $server_dir.old ; mv $server_dir.new $server_dir; rm -rf $server_dir.old"
+# shellcheck disable=SC2029
+ssh "$ssh_opts" "$SSH_USER@$SSH_HOST" "mv $dest $dest.old ; mv $dest.new $dest; rm -rf $dest.old"
 
-echo "Site is https://$SSH_HOST/$SITE_NAME"
-
+echo "Site is https://$SPEC_SERVER/$SITE_NAME"
