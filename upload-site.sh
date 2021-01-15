96abaaac6db0317d3db5b1a24b32e5f5f967fbfd
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-15 18:14:33 +0000

    Upload to SITE_NAME

--- upload-site.sh
+++ upload-site.sh
@@ -3,12 +3,8 @@
 # shellcheck source=get-config.sh
 . .scripts/get-config.sh
 
-# server_host=specs.nmos.bbctest01.uk
-# server_dir="/var/www/$server_host/html/$REPO_NAME"
-
-# server_host=specs.amwa.tv
 server_host=specs.amwa.tv
-server_dir="/var/www/$server_host/$REPO_NAME"
+server_dir="/var/www/$server_host/$SITE_NAME"
 
 # CHANGE THIS BEFORE PUSHING
 ssh_user=nmosmachine
@@ -20,4 +16,4 @@ ssh -i "$ssh_id" "$ssh_user@$server_host" rm -rf "$server_dir"
 echo "Uploading new site..."
 scp -q -i "$ssh_id" -r _site "$ssh_user@$server_host:$server_dir"
 
-echo "Site is https://$server_host/$REPO_NAME"
+echo "Site is https://$server_host/$SITE_NAME"
