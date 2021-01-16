48aa26696ae3417a5d934317d53210007bf9dc17
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-16 12:08:05 +0000

    Trying installing setuptools first

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,5 +17,6 @@ rm -rf raml2html-nmos-theme
 git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
 cp .scripts/package.json .
 yarn install
-pip3 install setuptools jsonref pathlib
+pip3 install setuptools
+pip3 install jsonref pathlib
 bundle install
