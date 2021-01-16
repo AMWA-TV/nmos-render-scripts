1f2a4234b5d199ac1a962aed021dfa22971f2d87
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-16 17:18:25 +0000

    make-site.sh removes source_repo and markdown

--- make-site.sh
+++ make-site.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+echo Removing source_repo...
+rm -rf source_repo 
+
 echo Building site...
 bundle exec jekyll build
+
+echo Removing Markdown...
+find _site -name '*.md' -exec rm {} \;
