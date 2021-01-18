0024a9c18fed3ff9855ab42821f90d74959af3b5
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-18 22:16:03 +0000

    Starting on moving render into dir

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,13 +2,13 @@
 
 set -o errexit
 
-[ ! -e README.md ] && echo Run this from the top-level directory && exit 1
+[[ "${PWD##*/}" != "render" ]] && echo Run this from the top-level directory && exit 1
 
 # shellcheck source=get-config.sh
 . .scripts/get-config.sh
 
 # Just install these for specs that need them
-if [[ -d APIs ]]; then
+if [[ -d ../APIs ]]; then
     rm -rf raml2html-nmos-theme
     git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
     cp .scripts/package.json .
