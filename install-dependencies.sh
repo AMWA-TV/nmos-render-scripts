ef24968fbb575fbb9794c82ee21f6a67a82b5e50
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-18 16:28:45 +0000

    All sites need bundle (Jekyll)

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,15 +8,15 @@ set -o errexit
 . .scripts/get-config.sh
 
 
-if [[ ! "$AMWA_ID" =~ "IS-" ]]; then
-    echo Nothing to install
-    exit 0
+# Many things only relate to IS- specs
+if [[ "$AMWA_ID" =~ "IS-" ]]; then
+    rm -rf raml2html-nmos-theme
+    git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
+    cp .scripts/package.json .
+    yarn install
+    pip3 install setuptools
+    pip3 install jsonref pathlib
 fi
 
-rm -rf raml2html-nmos-theme
-git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
-cp .scripts/package.json .
-yarn install
-pip3 install setuptools
-pip3 install jsonref pathlib
+# All specs need Jekull etc.
 bundle install
