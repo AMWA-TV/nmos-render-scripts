d81847a7bc63d935a3139cf2259120ad0014184a
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-02-28 11:37:07 +0000

    Copy .htaccess for SPECS repo

--- extract-docs.sh
+++ extract-docs.sh
@@ -63,6 +63,13 @@ function extract {
         cd source-repo || exit 1
         git checkout "$checkout"
 
+        # 404 doc for specs repo
+        if [[ "$AMWA_ID" == "SPECS" ]]; then
+            cp 404.md "../$target_dir"
+            cp .htaccess "../$target_dir"
+        fi
+
+        # Param regs still a special case
         if [[ "$AMWA_ID" == "NMOS-PARAMETER-REGISTERS" ]]; then
             cp -r common device-control-types device-types formats node-service-types tags transports "../$target_dir"
 
