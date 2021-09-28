095e77bc746c32a2945ce359b14e7eab2d68e204
Sonal Tandon <sonal.tandon@bbc.co.uk>
2021-09-28 18:07:22 +0100

    Removing local package installation

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,14 +4,5 @@ set -o errexit
 
 [[ "${PWD##*/}" != ".render" ]] && echo Run this from the top-level directory && exit 1
 
-# Just install these for specs that need them
-#if [[ -d ../APIs || -d ../examples ]]; then
-#    rm -rf raml2html-nmos-theme
-#    git clone https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/raml2html-nmos-theme
-#    cp .scripts/package.json .
-#    yarn install
-#fi
-
-
 # shellcheck source=get-config.sh
 . .scripts/get-config.sh
