74b776c726f97d798e1302ba7086212eb74bbb9b
Sonal Tandon <sonal.tandon@bbc.co.uk>
2021-09-28 18:33:08 +0100

    Removing -s flag

--- make-indexes.sh
+++ make-indexes.sh
@@ -210,7 +210,7 @@ echo "Making top level $INDEX"
     elif [[ "$AMWA_ID" != "SPECS" ]]; then
         echo -e "\n\n## About ${AMWA_ID}\n\n"
     fi
-    ed -s "$TOP_README" <<< '/INTRO-START/+1,/INTRO-END/-1p'
+    ed "$TOP_README" <<< '/INTRO-START/+1,/INTRO-END/-1p'
     echo -e "\n\n---\n\n"
 } > "$INDEX"
 
