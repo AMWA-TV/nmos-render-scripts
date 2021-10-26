67384af02742f9c8cd71e8c4f60350d6d0a49962
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-10-26 21:37:07 +0100

    Update entrypoint.sh
    Clone default branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 
-git clone --single-branch --branch main "https://github.com/$GITHUB_REPOSITORY" /github-repo
+git clone --single-branch "https://github.com/$GITHUB_REPOSITORY" /github-repo
 cd /github-repo/.render || exit
 make build-tools
 make build
