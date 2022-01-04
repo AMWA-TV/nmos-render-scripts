d63255f6080161324144e2edfd54d35f163e909e
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-01-04 11:44:20 +0000

    Get specs.json for dynamic menus

--- make-layouts.sh
+++ make-layouts.sh
@@ -1,6 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# shellcheck source=get-config.sh
+. "$(dirname "${BASH_SOURCE[0]}")/get-config.sh"
 
 set -o errexit
+
+# NMOS specs need to get specs.json from the index to populate their menus
+if [[ "$AMWA_ID" != "SPECS" && "$AMWA_ID" != "NMOS" ]]; then
+	echo Getting specs.json
+	wget -O- -q https://specs.amwa.tv/nmos/specs.json > _data/specs.json
+fi
 
 git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-layouts .layouts
 rm -rf _layouts assets
