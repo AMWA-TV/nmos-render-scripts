51cabba5e22bf372bed8d7577111409e5f39acfd
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-01-04 12:14:22 +0000

    Copy _includes from layouts

--- make-layouts.sh
+++ make-layouts.sh
@@ -5,14 +5,18 @@
 
 set -o errexit
 
-# NMOS specs need to get specs.json from the index to populate their menus
-if [[ "$AMWA_ID" != "SPECS" && "$AMWA_ID" != "NMOS" ]]; then
-	echo Getting specs.json
-	wget -O- -q https://specs.amwa.tv/nmos/specs.json > _data/specs.json
-fi
+echo Setting up layouts
 
 git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-layouts .layouts
 rm -rf _layouts assets
 mv .layouts/_layouts .
 mv .layouts/assets .
+[[ ! -d _includes ]] && mkdir _includes # nmos repo already has it
+mv .layouts/_includes/* _includes/
 rm -rf .layouts
+
+# NMOS specs need to get specs.json from the index to populate their menus
+if [[ "$AMWA_ID" != "SPECS" && "$AMWA_ID" != "NMOS" ]]; then
+	echo Getting specs.json
+	wget -O- -q https://specs.amwa.tv/nmos/specs.json > _data/specs.json
+fi
