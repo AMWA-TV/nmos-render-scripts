726d9c91f9f1dcdf0a1ae52de3083fe00bc57ac0
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-01-07 20:37:30 +0000

    Reworking make-layouts

--- make-layouts.sh
+++ make-layouts.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# Make the dirs required for Jekyll, copy common files from layouts repo, 
+# and copy any local files, overwriting as required. 
+
 set -o errexit
 
 echo Setting up layouts
 
-git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-layouts .layouts
-rm -rf _layouts assets
-mv .layouts/_layouts .
-mv .layouts/assets .
-[[ ! -d _includes ]] && mkdir _includes # nmos repo already has it
-mv .layouts/_includes/* _includes/
-rm -rf .layouts
+if [[ -d .layouts ]]; then
+	echo "Warning: .layouts exists so not cloning"
+else
+	git clone --single-branch --branch "${NMOS_DOC_LAYOUTS_BRANCH:-main}" https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-layouts .layouts
+fi
+
+for dir in _layouts _includes assets assets/css assets/images; do
+	cp -r ".layouts/$dir" "$dir"
+	[[ -d "_local/$dir" ]] && cp -r "_local/$dir"/* "$dir"
+done
+
+exit 0
