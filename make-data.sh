76a3ebe8fec19ebacafafdf348122f09b31c145f
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-01-08 23:34:27 +0000

    Added apis, schemas, examples, branches, releases json

--- make-data.sh
+++ make-data.sh
@@ -5,6 +5,15 @@
 
 set -o errexit
 
+function make_json_page {
+	cat <<EOF > "$1.json"
+---
+---
+
+{{ site.data.$1 | jsonify }}
+EOF
+}
+
 echo Creating _data files
 
 [[ ! -d _data ]] && mkdir _data
@@ -30,5 +39,77 @@ if [[ -d "$DEFAULT_TREE/docs" ]]; then
 	awk -F'^ *- \\[.*\\]\\(' '(NF>1){print $2}' "$DEFAULT_TREE/docs/README.md" | \
 	sed 's/\.html)$//' | \
 	jq -R -n '[inputs]' | \
-	jq '[.[] | {name: . | sub("_";" "), file: "\(.).html" }]' > _data/docs.json
+	jq '[.[] | {name: . | sub("_";" "), file: "\(.).html"}]' > _data/docs.json
+	make_json_page docs
 fi
+
+if [[ -d "$DEFAULT_TREE/APIs" ]]; then
+	echo Making apis.json
+	(
+		cd "$DEFAULT_TREE/APIs" || exit 1
+		for file in *.html; do
+			echo "${file%.html}"
+		done
+	) | \
+	jq -R -n '[inputs]'	| \
+	jq '[.[] | {name: . | sub("API$"; " API"), file: "\(.).html"}]' > _data/apis.json
+	make_json_page apis
+fi
+
+if [[ -d "$DEFAULT_TREE/APIs/schemas/with-refs" ]]; then
+	echo Making schemas.json
+	(
+		cd "$DEFAULT_TREE/APIs/schemas/with-refs" || exit 1
+		for file in *.html; do
+			echo "${file%.html}"
+		done
+	) | \
+	jq -R -n '[inputs]'	| \
+	jq '[.[] | {name: ., file: "\(.).html"}]' > _data/schemas.json
+	make_json_page schemas
+fi
+
+if [[ -d "$DEFAULT_TREE/examples" ]]; then
+	echo Making examples.json
+	(
+		cd "$DEFAULT_TREE/examples" || exit 1
+		for file in *.html; do
+			echo "${file%.html}"
+		done
+	) | \
+	jq -R -n '[inputs]'	| \
+	jq '[.[] | {name: ., file: "\(.).html"}]' > _data/examples.json
+	make_json_page examples
+fi
+
+if [[ -d "branches" ]]; then
+	echo Making branches.json
+	(
+		cd branches || exit 1
+		for branch in *; do
+			if [[ "$branch" != "index.md" ]]; then
+				echo "$branch"
+			fi
+		done
+	) | \
+	jq -R -n '[inputs]'	| \
+	jq '[.[] | {name: .}]' > _data/branches.json
+	make_json_page branches
+fi
+
+if [[ -d "releases" ]]; then
+	echo Making releases.json
+	(
+		cd releases || exit 1
+		for release in *; do
+			if [[ "$release" != "index.md" ]]; then
+				echo "$release"
+			fi
+		done
+	) | \
+	jq -R -n '[inputs]'	| \
+	jq '[.[] | {name: .}]' > _data/releases.json
+	make_json_page releases
+fi
+
+exit 0
