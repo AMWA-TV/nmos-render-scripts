0f1a97636675b1e3bc558cc34ab302c1c9dafd5b
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-06-24 12:14:52 +0100

    Fix liquid exception 'No repo name found.'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+export PAGES_REPO_NWO="$GITHUB_REPOSITORY"
 git clone --single-branch "https://github.com/$GITHUB_REPOSITORY" /github-repo
 cd /github-repo/.render || exit
 chmod 777 .
