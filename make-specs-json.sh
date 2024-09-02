c2bf39cf2a97a583aa9f79106f868bacacdb6df1
Peter Brightwell <peter.brightwell@bbc.co.uk>
2024-09-02 20:22:53 +0100

    Pass lint

--- make-specs-json.sh
+++ make-specs-json.sh
@@ -10,16 +10,12 @@ echo "Getting spec info"
 for id in $(yaml2json ../spec_list.yml | jq -r '.[]'); do
     echo "$id"
     # themes for this id to mix in
-    id_themes=$(echo $themes | jq "[ .[] | select(.members[] == \"$id\").name ]")
+    id_themes=$(echo "$themes" | jq "[ .[] | select(.members[] == \"$id\").name ]")
     wget -O- -q "https://specs.amwa.tv/${id,,}/spec.json" | jq ".themes = $id_themes" >> "$TMP/specs"
 done
 echo
 
 echo "Making specs.json"
 jq --slurp . "$TMP/specs" > _data/specs.json
-# echo "Making specs-by-theme.json"
-# for theme in $(yaml2json ../themes.yml | jq -r '.[]'); do
-#     echo "$theme"
-# done
 
 rm -r "$TMP"
