03580a978d0c742ae0e561049c34a933ed86dbe2
Peter Brightwell <peter.brightwell@bbc.co.uk>
2024-09-03 15:56:35 +0100

    Themes now have id, name and description

--- make-specs-json.sh
+++ make-specs-json.sh
@@ -10,7 +10,7 @@ echo "Getting spec info"
 for id in $(yaml2json ../spec_list.yml | jq -r '.[]'); do
     echo "$id"
     # themes for this id to mix in
-    id_themes=$(echo "$themes" | jq "[ .[] | select(.members[] == \"$id\").name ]")
+    id_themes=$(echo "$themes" | jq "[ .[] | select(.members[] == \"$id\").id ]")
     wget -O- -q "https://specs.amwa.tv/${id,,}/spec.json" | jq ".themes = $id_themes" >> "$TMP/specs"
 done
 echo
